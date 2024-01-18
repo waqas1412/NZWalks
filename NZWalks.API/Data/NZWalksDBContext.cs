6288181097c51afb1296b77d39f19db4329b8ecf
Waqas <muhammad.waqas1412@gmail.com>
2024-01-18 14:38:25 +0500

    EF Migration

--- NZWalks.API/Data/NZWalksDBContext.cs
+++ NZWalks.API/Data/NZWalksDBContext.cs
@@ -5,7 +5,7 @@ namespace NZWalks.API.Data
 {
     public class NZWalksDBContext : DbContext
     {
-        public NZWalksDBContext(DbContextOptions dbContextOptions): base(dbContextOptions)
+        public NZWalksDBContext(DbContextOptions<NZWalksDBContext> dbContextOptions): base(dbContextOptions)
         {
             
         }
