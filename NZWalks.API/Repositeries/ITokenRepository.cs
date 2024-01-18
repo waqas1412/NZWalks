1c2c0e7a07b826325c84ec81460310d533cfe63d
Waqas <muhammad.waqas1412@gmail.com>
2024-01-18 15:27:30 +0500

    Creating Token Functionality

---
+++ NZWalks.API/Repositeries/ITokenRepository.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace NZWalks.API.Repositeries
+{
+    public interface ITokenRepository
+    {
+        string CreateJWTToken(IdentityUser user, List<string> roles);
+    }
+}
